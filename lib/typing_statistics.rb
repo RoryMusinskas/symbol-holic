859b4ec1e41dbc5dc29fb4602ab274989103c9c4
RoryMusinskas <rorymuzz@gmail.com>
2020-09-26 12:12:08 +1000

    Added in persistant data storage and associated methods to read and write to the JSON file. Removed the now redundant method and reformatted code slightly. Added comments to keep track during development.

--- lib/typing_statistics.rb
+++ lib/typing_statistics.rb
@@ -1,31 +1,62 @@
+require 'json'
+require 'terminal-table'
 require_relative 'typing_game'
 require_relative 'string_generator'
-require 'terminal-table'
 
 class TypingStatistics
+  # Bring in the string generator module
   include StringGenerator
 
-  def self.stats(scores, times_played)
-    @combined_scores = StringGenerator.add_symbols_to_hash if times_played == 1
-    scores.each do |key, val|
-      # Storing the average WPM to the hash
-      @combined_scores[key][0] = ((@combined_scores[key][0] + val[0]) / (@combined_scores[key][2] + val[2]))
-      # Storing the average accuracy to the hash
-      @combined_scores[key][1] = ((@combined_scores[key][1] + val[1]) / (@combined_scores[key][2] + val[2]))
-      # Storing the amount of times each symbol has been displayed
-      @combined_scores[key][2] = (@combined_scores[key][2] + val[2])
+  # The method for taking in the score of the game that the user just played
+  # It then inputs the score into the average statistics for the user
+  def self.statistics(scores)
+    # If there is already a hash in the JSON File, then read it or else create a new blank hash (Will only be first time user runs game)
+    begin
+      read_statistics
+    rescue StandardError
+      # Create an empty hash so the score can be appended to it, as it will be nil otherwise and throw an error
+      @averaged_statistics = StringGenerator.add_symbols_to_hash if @averaged_statistics.nil?
     end
-    @combined_scores
+
+    # Take the scores hash of the last completed typing game and append them to the averaged_statistics hash
+    scores.each do |key, val|
+      # Storing the average WPM to the averaged hash
+      @averaged_statistics[key][0] = ((@averaged_statistics[key][0] + val[0]) / (@averaged_statistics[key][2] + val[2]))
+      # Storing the average accuracy to the averaged hash
+      @averaged_statistics[key][1] = ((@averaged_statistics[key][1] + val[1]) / (@averaged_statistics[key][2] + val[2]))
+      # Storing the amount of times each symbol has been displayed to the averaged hash
+      @averaged_statistics[key][2] = (@averaged_statistics[key][2] + val[2])
+    end
+    # Write the statistics at the end of each game into the JSON file
+    write_statistics(@averaged_statistics)
   end
 
-  def self.display_scores
-    # p @combined_scores
-    hash_to_array
+  # Used for getting the absolute file path, wherever the user is. For when the user runs program from anywhere.
+  def self.file_path
+    path = File.dirname(__FILE__).split('/')
+    path.pop
+    json_file = "#{path.join('/')}/public/bookmarks.json"
+    json_file
   end
 
-  def self.hash_to_array
+  # Read and parse the averaged_statistics hash from the JSON file
+  def self.read_statistics
+    file = File.read(file_path)
+    @averaged_statistics = JSON.parse(file)
+  end
+
+  # Write the averaged_statistics hash to the JSON file
+  def self.write_statistics(averaged_statistics)
+    File.write(file_path, JSON.pretty_generate(averaged_statistics))
+  end
+
+  # Display the statistics in a terminal table, for the users to read.
+  def self.display_statistics
+    # Read the current JSON file to get the averaged_statistics hash
+    read_statistics
+    # Make a terminal table and add in values from averaged_statistics hash
     @rows = []
-    @combined_scores.each do |key, val|
+    @averaged_statistics.each do |key, val|
       @rows << [key, val[0], val[1], val[2]]
       @rows << :separator
     end
