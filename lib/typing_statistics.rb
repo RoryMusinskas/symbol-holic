948a3c2201674335342a11b5e0afd4201cffe249
RoryMusinskas <rorymuzz@gmail.com>
2020-09-26 16:40:17 +1000

    Redesigned the storing of the average scores in the hash for better readability, rearranged the terminal table to reflect this and for better debugging at this stage.

--- lib/typing_statistics.rb
+++ lib/typing_statistics.rb
@@ -2,14 +2,14 @@ require 'json'
 require 'terminal-table'
 require_relative 'typing_game'
 require_relative 'string_generator'
-
+require 'byebug'
 class TypingStatistics
   # Bring in the string generator module
   include StringGenerator
 
   # The method for taking in the score of the game that the user just played
   # It then inputs the score into the average statistics for the user
-  def self.statistics(scores)
+  def statistics(scores)
     # If there is already a hash in the JSON File, then read it or else create a new blank hash (Will only be first time user runs game)
     begin
       read_statistics
@@ -20,47 +20,50 @@ class TypingStatistics
 
     # Take the scores hash of the last completed typing game and append them to the averaged_statistics hash
     scores.each do |key, val|
-      # Storing the average WPM to the averaged hash
-      @averaged_statistics[key][0] = ((@averaged_statistics[key][0] + val[0]) / (@averaged_statistics[key][2] + val[2]))
-      # Storing the average accuracy to the averaged hash
-      @averaged_statistics[key][1] = ((@averaged_statistics[key][1] + val[1]) / (@averaged_statistics[key][2] + val[2]))
       # Storing the amount of times each symbol has been displayed to the averaged hash
-      @averaged_statistics[key][2] = (@averaged_statistics[key][2] + val[2])
+      @averaged_statistics[key][0] = @averaged_statistics[key][0] + val[0]
+      # Storing the total amount of errors the user has made
+      @averaged_statistics[key][1] = @averaged_statistics[key][1] + val[1]
+      # Storing the average accuracy for the user
+      @averaged_statistics[key][2] = @averaged_statistics[key][0] / (@averaged_statistics[key][0] + @averaged_statistics[key][1]).to_f
+      # Storing the average WPM to the averaged hash
+      @averaged_statistics[key][3] = ((@averaged_statistics[key][3] + val[2]) / (@averaged_statistics[key][0]))
     end
     # Write the statistics at the end of each game into the JSON file
     write_statistics(@averaged_statistics)
   end
 
   # Used for getting the absolute file path, wherever the user is. For when the user runs program from anywhere.
-  def self.file_path
+  def file_path
     path = File.dirname(__FILE__).split('/')
     path.pop
-    json_file = "#{path.join('/')}/public/bookmarks.json"
+    json_file = "#{path.join('/')}/public/key_scores.json"
     json_file
   end
 
   # Read and parse the averaged_statistics hash from the JSON file
-  def self.read_statistics
+  def read_statistics
     file = File.read(file_path)
     @averaged_statistics = JSON.parse(file)
   end
 
   # Write the averaged_statistics hash to the JSON file
-  def self.write_statistics(averaged_statistics)
+  def write_statistics(averaged_statistics)
     File.write(file_path, JSON.pretty_generate(averaged_statistics))
   end
 
   # Display the statistics in a terminal table, for the users to read.
-  def self.display_statistics
+  def display_statistics
     # Read the current JSON file to get the averaged_statistics hash
     read_statistics
     # Make a terminal table and add in values from averaged_statistics hash
     @rows = []
     @averaged_statistics.each do |key, val|
-      @rows << [key, val[0], val[1], val[2]]
+      @rows << [key, val[3], val[2], val[0], val[1]]
+      # @rows << [key, val[0], val[1], val[2], val[3]]
       @rows << :separator
     end
-    table = Terminal::Table.new headings: ['Symbol', 'Average WPM', 'Average Accuracy', 'Symbol Count'], rows: @rows
+    table = Terminal::Table.new headings: ['Symbol', 'Average WPM', 'Average Accuracy', 'Symbol Count', 'Errors'], rows: @rows
     puts table
   end
 end
