b82b613b73207fecde7bb157d4a8053927573827
RoryMusinskas <rorymuzz@gmail.com>
2020-09-26 16:35:13 +1000

    Added instances of the typing game and the typing statistics for it to be more object orinented' ~

--- lib/menu.rb
+++ lib/menu.rb
@@ -4,11 +4,6 @@ require_relative 'typing_game'
 require_relative 'typing_statistics'
 
 class Menu
-  def initialize
-    # If it is initialized up here then it doesn't generate a new array/object each time
-    # @typing_game = TypingGame.new
-  end
-
   def menu_selection
     # Choices for the user to select at the menu
     menu_choices = [
@@ -25,15 +20,18 @@ class Menu
       choice = menu_selection.to_i
       # Start typing game if user selects 'Start Typing'
       if choice == 1
-        @scores = TypingGame.new.run_game
-        TypingStatistics.statistics(@scores)
+        @typing_game = TypingGame.new
+        @scores = @typing_game.run_game
+        @typing_statistics = TypingStatistics.new
+        @typing_statistics.statistics(@scores)
+      # TypingStatistics.statistics(@scores)
       # Display the users statistics if they select 'Display your typing statistics'
       elsif choice == 2
         # If the user hasn't played a game yet, there is no need to display their typing scores
         # It throws an error as the Class is nil at that stage
         begin
           # If it is not nil, display their statistics
-          TypingStatistics.display_statistics
+          @typing_statistics.display_statistics
           # If it is nil, display a prompt and let them choose another menu item
         rescue StandardError
           puts
