d12cce041971a93ac059afda7ee72972f3fdf7b1
RoryMusinskas <rorymuzz@gmail.com>
2020-09-26 12:08:04 +1000

    Removed the times played variable, as it was redundant with persistant data storage. Added in error handling for the menu selection, users cant view scores if they havent played yet. Added comments to keep track of development

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,4 +1,5 @@
 require 'tty-prompt'
+require 'json'
 require_relative 'typing_game'
 require_relative 'typing_statistics'
 
@@ -6,14 +7,13 @@ class Menu
   def initialize
     # If it is initialized up here then it doesn't generate a new array/object each time
     # @typing_game = TypingGame.new
-    @times_played = 0
   end
 
   def menu_selection
     # Choices for the user to select at the menu
     menu_choices = [
       { name: 'Start Typing', value: '1' },
-      { name: 'View your typing speeds', value: '2' },
+      { name: 'Display your typing statistics', value: '2' },
       { name: 'Exit', value: '3' }
     ]
     # TTY prompt with the menu choices, users can select one of these items
@@ -23,12 +23,24 @@ class Menu
   def display_menu
     loop do
       choice = menu_selection.to_i
+      # Start typing game if user selects 'Start Typing'
       if choice == 1
-        @times_played += 1
         @scores = TypingGame.new.run_game
-        TypingStatistics.stats(@scores, @times_played)
+        TypingStatistics.statistics(@scores)
+      # Display the users statistics if they select 'Display your typing statistics'
       elsif choice == 2
-        TypingStatistics.display_scores
+        # If the user hasn't played a game yet, there is no need to display their typing scores
+        # It throws an error as the Class is nil at that stage
+        begin
+          # If it is not nil, display their statistics
+          TypingStatistics.display_statistics
+          # If it is nil, display a prompt and let them choose another menu item
+        rescue StandardError
+          puts
+          puts "You haven't played a game yet."
+          puts
+        end
+        # Exit the program if the user selects 'Exit'
       elsif choice == 3
         exit
       end
