6a3b50a0d3fe64f755d988509cda056526e04b0a
RoryMusinskas <rorymuzz@gmail.com>
2020-09-27 20:15:03 +1000

    Added in the menu option for resetting the users data. Also formatted document a bit better.

--- lib/menu.rb
+++ lib/menu.rb
@@ -14,10 +14,31 @@ class Menu
     menu_choices = [
       { name: 'Start Typing', value: '1' },
       { name: 'Display your typing statistics', value: '2' },
-      { name: 'Exit', value: '3' }
+      { name: 'Reset your typing statistics', value: '3' },
+      { name: 'Exit', value: '4' }
     ]
     # TTY prompt with the menu choices, users can select one of these items
     TTY::Prompt.new.select('Welcome to Symbol-holic!', menu_choices)
+  end
+
+  # This is the loop which runs and displays the menu to the user
+  def display_menu
+    loop do
+      choice = menu_selection.to_i
+      # Start typing game if user selects 'Start Typing'
+      if choice == 1
+        run_game
+      # Display the users statistics if they select 'Display your typing statistics'
+      elsif choice == 2
+        display_statistics
+      # Reset users statistics if they want to
+      elsif choice == 3
+        reset_statistics
+        # Exit the program if the user selects 'Exit'
+      elsif choice == 4
+        exit
+      end
+    end
   end
 
   # Added in for the command line arguments. We can call display_statistics on menu and not create a new TypingStatistics instance
@@ -40,19 +61,17 @@ class Menu
     @typing_statistics.statistics(@scores)
   end
 
-  def display_menu
-    loop do
-      choice = menu_selection.to_i
-      # Start typing game if user selects 'Start Typing'
-      if choice == 1
-        run_game
-      # Display the users statistics if they select 'Display your typing statistics'
-      elsif choice == 2
-        display_statistics
-        # Exit the program if the user selects 'Exit'
-      elsif choice == 3
-        exit
-      end
+  # Menu option and TTY prompt for deleting all typing statistics
+  def reset_statistics
+    reset_choices = [
+      { name: 'Yes', value: '1' },
+      { name: 'No', value: '2' }
+    ]
+    choice = TTY::Prompt.new.select('Are you sure?', reset_choices).to_i
+    if choice == 1
+      @typing_statistics.wipe_file
+    elsif choice == 2
+      display_menu
     end
   end
 end
