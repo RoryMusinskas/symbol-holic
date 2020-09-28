8562efb8bd34d6e79c366e1b5fef91f0133d0597
RoryMusinskas <rorymuzz@gmail.com>
2020-09-28 14:38:15 +1000

    Added in the logic for creating a randomized array and merging it back together for the users worst 8 keys. This is for the targeted typing game mode.

--- lib/statistics_helper.rb
+++ lib/statistics_helper.rb
@@ -1,4 +1,6 @@
 require 'byebug'
+require_relative 'typing_statistics'
+
 module StatisticsHelper
   # An array of all the symbols to be availble to the user
   SYMBOLS = ['#', '`', '%', '&', '[', '{', '}', '(', '=', '*', ')', '+', ']', '!', '|', '-', '_', '@', '^', '/', '?', '<', '>', '$', '~', ';', ':'].freeze
@@ -13,6 +15,28 @@ module StatisticsHelper
     end
     # ensure that all elements in the array are unique
     randomized_array
+  end
+
+  # This is a method to get the last 8 keys from the user to make into a new array for targeted practice
+  def self.create_targeted_array
+    worst_key_array = []
+    # Read the users statistics from the file
+    typing_statistics = TypingStatistics.new.read_statistics
+    # Get the last 8 hash elements (The users worst keys), by revesing hash and getting the first 8 elements
+    worst_keys = typing_statistics.reverse_each.to_h.first(8)
+    worst_keys.each do |key|
+      worst_key_array << key[0]
+    end
+    worst_key_array
+  end
+
+  # This method creates a randomized array with the worst 8 keys, all represented 3 times and shuffled.
+  def self.randomized_targeted_array
+    randomized_array = []
+    3.times do
+      randomized_array << create_targeted_array.shuffle
+    end
+    randomized_array.flatten
   end
 
   # This is called when there is no data stored in the JSON file. It outputs a hash with all the symbols and sets all values to 0
