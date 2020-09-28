5ff4e70593624d27ef48796b17a8e729f2f7a544
RoryMusinskas <rorymuzz@gmail.com>
2020-09-28 14:40:03 +1000

    Added in the method to run the targeted typing game. Moved the game logic to it's own method so the normal game and the targeted game can both call it independently

--- lib/typing_game.rb
+++ lib/typing_game.rb
@@ -10,41 +10,53 @@ class TypingGame
   # Initialize the typing game
   def initialize
     @symbol_array = StatisticsHelper.create_symbol_array
+    @targeted_array = StatisticsHelper.randomized_targeted_array
     @keypress_statistics = {}
   end
 
   # Calculate the words per minute of each keypress the user types
   def words_per_min(first_time, second_time)
-    @words_per_min = 60 / (second_time.round(1) - first_time.round(1))
+    @words_per_min = 60 / (second_time - first_time)
   end
 
-  # The logic for running the game
   def run_game
     # Print each item from the generated symbol array
     @symbol_array.each do |item|
-      @wrong_keys = 0
-      print "#{item}  --->   "
-
-      # Get the time at the start of each loop
-      first_time = Time.now.to_f
-      # Get the current user input key, without having to press enter
-      current_input = STDIN.getch
-
-      # Until the user gets the right key, keep prompting them, add keep a total for the amount of wrong keys hit before the correct one
-      while current_input != item
-        current_input = STDIN.getch
-        @wrong_keys += 1
-      end
-
-      # Get the time when the user types the correct key
-      second_time = Time.now.to_f if current_input == item
-      puts current_input
-
-      # Call and return the word per minute of each symbol
-      words_per_min(first_time, second_time)
-      # add the words per min, accuracy and 1 (for the amount of times the symbol has been shown)
-      @keypress_statistics[item] = [1, @wrong_keys, @words_per_min]
+      game_logic(item)
     end
     @keypress_statistics
+  end
+
+  def targeted_game
+    @targeted_array.each do |item|
+      game_logic(item)
+    end
+    @keypress_statistics
+  end
+
+  # The logic for running the game
+  def game_logic(item)
+    @wrong_keys = 0
+    print "#{item}  --->   "
+
+    # Get the time at the start of each loop
+    first_time = Time.new.to_f
+    # Get the current user input key, without having to press enter
+    current_input = STDIN.getch
+
+    # Until the user gets the right key, keep prompting them, add keep a total for the amount of wrong keys hit before the correct one
+    while current_input != item
+      current_input = STDIN.getch
+      @wrong_keys += 1
+    end
+
+    # Get the time when the user types the correct key
+    second_time = Time.new.to_f if current_input == item
+    puts current_input
+
+    # Call and return the word per minute of each symbol
+    words_per_min(first_time, second_time)
+    # add the words per min, accuracy and 1 (for the amount of times the symbol has been shown)
+    @keypress_statistics[item] = [1, @wrong_keys, @words_per_min]
   end
 end
