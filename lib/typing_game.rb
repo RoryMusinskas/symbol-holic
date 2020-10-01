6a1fe3afa6f27643bee5f13d558276c402e63cc4
RoryMusinskas <rorymuzz@gmail.com>
2020-10-01 13:54:10 +1000

    Changed some methods after testing, they are more succinct and better to test setup like this

--- lib/typing_game.rb
+++ lib/typing_game.rb
@@ -5,18 +5,13 @@ require_relative 'statistics_helper'
 
 class TypingGame
   # Bring in the StatisticsHelper module
-  include StatisticsHelper
+  # include StatisticsHelper
 
   # Initialize the typing game
   def initialize
     @symbol_array = StatisticsHelper.create_symbol_array
     @targeted_array = StatisticsHelper.randomized_targeted_array
     @keypress_statistics = {}
-  end
-
-  # Calculate the words per minute of each keypress the user types
-  def words_per_min(first_time, second_time)
-    @words_per_min = 60 / (second_time - first_time)
   end
 
   # Run the stardard typing game
@@ -36,17 +31,23 @@ class TypingGame
     @keypress_statistics
   end
 
+  # Calculate the words per minute of each keypress the user types
+  def words_per_min(first_time, second_time)
+    words_per_min = 60 / (second_time - first_time)
+    words_per_min
+  end
+
   # Calculate the statistics to put into the hash, need to add the values each loop now that you can do multiple keys each run
-  def calculate_statistics(item, wrong_keys)
+  def calculate_statistics(item, wrong_keys, words_per_min)
     # If there is no value for the key in the hash, then add the current values in
     if @keypress_statistics[item].nil?
       # add the words per min, accuracy and 1 (for the amount of times the symbol has been shown)
-      @keypress_statistics[item] = [1, wrong_keys, @words_per_min]
+      @keypress_statistics[item] = [1, wrong_keys, words_per_min]
     else
       # Get the values in the hash and add the values to make a total to pass to the statistics
       symbol_count = @keypress_statistics[item][0] + 1
       wrong_keys_count = @keypress_statistics[item][1] + wrong_keys
-      wpm_total = @keypress_statistics[item][2] + @words_per_min
+      wpm_total = @keypress_statistics[item][2] + words_per_min
 
       @keypress_statistics[item] = [symbol_count, wrong_keys_count, wpm_total]
     end
@@ -73,8 +74,8 @@ class TypingGame
     second_time = Time.new.to_f if current_input == item
     puts current_input
 
-    # Call and return the word per minute of each symbol
-    words_per_min(first_time, second_time)
-    calculate_statistics(item, wrong_keys)
+    # Call and return the words per minute of each symbol
+    # The game logic returns the calculated statistics of each key each loop
+    calculate_statistics(item, wrong_keys, words_per_min(first_time, second_time))
   end
 end
