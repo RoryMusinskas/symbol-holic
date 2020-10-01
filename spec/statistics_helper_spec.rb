5c91d214fa484f2e142ffe5cf31f23c544d9599e
RoryMusinskas <rorymuzz@gmail.com>
2020-10-01 13:59:00 +1000

    Completed testing for creating a randomized array and for the targeted array

--- spec/statistics_helper_spec.rb
+++ spec/statistics_helper_spec.rb
@@ -1,10 +1,10 @@
-require_relative '../lib/statistics_helper'
+require_relative '../lib/statistics_helper.rb'
 
 describe 'Create Symbol Array' do
   it 'should return an array that is not empty' do
     expect(StatisticsHelper.create_symbol_array.empty?).to be(false)
   end
-  it 'should return an array of symbols that is the same length as the random number' do
+  it 'should return an array of symbols that has 15 elements' do
     expect(StatisticsHelper.create_symbol_array.length).to be(15)
   end
   it 'should only have the provided symbols in the array' do
@@ -16,6 +16,34 @@ describe 'Create Symbol Array' do
       is_included
     end
     expect(only_approved_symbols).to be(true)
+  end
+end
+
+describe 'It should create a targeted array' do
+  it 'should return an array that is not empty' do
+    expect(StatisticsHelper.create_targeted_array.empty?).to be(false)
+  end
+  it 'should return an array of symbols that has 8 elements' do
+    expect(StatisticsHelper.create_targeted_array.length).to be(8)
+  end
+  it 'should only have the provided symbols in the array' do
+    def only_approved_symbols
+      is_included = true
+      StatisticsHelper.create_targeted_array.each do |item|
+        is_included = false if StatisticsHelper::SYMBOLS.include?(item) == false
+      end
+      is_included
+    end
+    expect(only_approved_symbols).to be(true)
+  end
+end
+
+describe 'It should return a randomized targeted array' do
+  it 'should return an array that is not empty' do
+    expect(StatisticsHelper.randomized_targeted_array.empty?).to be(false)
+  end
+  it 'should return an array with 24 elements' do
+    expect(StatisticsHelper.randomized_targeted_array.length).to be(24)
   end
 end
 
