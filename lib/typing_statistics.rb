00047e45e1111266b151d71a8fe1aa8408170103
RoryMusinskas <rorymuzz@gmail.com>
2020-10-01 13:57:22 +1000

    Added in sorting for the hash so the users can get their worst scores. Added in TTY-Pie logic to add in the pie chart for each symbol

--- lib/typing_statistics.rb
+++ lib/typing_statistics.rb
@@ -1,13 +1,14 @@
 require 'json'
 require 'terminal-table'
+require 'tty-pie'
 require 'colorize'
 require 'colorized_string'
 require_relative 'typing_game'
 require_relative 'statistics_helper'
-require 'byebug'
 class TypingStatistics
   # The method for taking in the score of the game that the user just played
   # It then inputs the score into the average statistics for the user
+  # include StatisticsHelper
   def statistics(scores)
     # If there is already a hash in the JSON File, then read it or else create a new blank hash (Will only be first time user runs game)
     begin
@@ -31,15 +32,7 @@ class TypingStatistics
       @averaged_statistics[key][4] = average_wpm(@averaged_statistics[key][3], @averaged_statistics[key][0])
     end
     # Write the statistics at the end of each game into the JSON file
-    write_statistics(sort_averaged_statistics)
-  end
-
-  # Sort the users average scores by adding the average speed and accuracy, this is the indication of a good key
-  def sort_averaged_statistics
-    # This converts the hash into a sorted array
-    sorted_array = @averaged_statistics.sort_by { |_key, val| -(val[2] + val[4]) }
-    # Convert the array back to a hash for processing
-    @averaged_statistics = sorted_array.to_h
+    write_statistics(sort_averaged_statistics(@averaged_statistics))
   end
 
   # Total counter method to assist with setting the key values
@@ -56,6 +49,14 @@ class TypingStatistics
   # Average Words Per Minute method to assist with setting the key values
   def average_wpm(total_wpm, total_symbol_count)
     (total_wpm / total_symbol_count).round(1)
+  end
+
+  # Sort the users average scores by adding the average speed and accuracy, this is the indication of a good key
+  def sort_averaged_statistics(averaged_statistics)
+    # This converts the hash into a sorted array
+    sorted_array = averaged_statistics.sort_by { |_key, val| -(val[2] + val[4]) }
+    # Convert the array back to a hash for processing
+    @averaged_statistics = sorted_array.to_h
   end
 
   # Used for getting the absolute file path, wherever the user is. For when the user runs program from anywhere.
@@ -77,6 +78,20 @@ class TypingStatistics
     File.write(file_path, JSON.pretty_generate(averaged_statistics))
   end
 
+  # Make a pie chart for the hit and miss count
+  def make_pie_chart(total_hit, total_miss)
+    data = [
+      { value: total_hit, color: :bright_green, fill: '•' },
+      { value: total_miss, color: :bright_red, fill: '•' }
+    ]
+    # Create the pie chart
+    pie_chart = TTY::Pie.new(data: data, radius: 2)
+    pie_chart.render
+    # It will throw a type error for nil values, in that case return a string
+  rescue TypeError
+    'no data yet'
+  end
+
   # Display the statistics in a terminal table, for the users to read.
   def display_statistics
     # Read the current JSON file to get the averaged_statistics hash
@@ -89,13 +104,18 @@ class TypingStatistics
       val[2] = val[2].to_s.colorize(:red) if val[2].positive? && val[2] < 60
       # Set the color of the WPM if the user WPM is under 50
       val[4] = val[4].to_s.colorize(:yellow) if val[4] < 40 && val[4] != 0
+
       # Add each symbol and associated data to the terminal-table row
-      @rows << [key, val[4], val[2], val[0]]
+      @rows << [key, val[4], val[2], val[0], make_pie_chart(val[0], val[1])]
       counter += 1
       # Print a seperator for each symbol apart from the last
       @rows << :separator if counter < @averaged_statistics.size
     end
-    table = Terminal::Table.new title: 'Your Typing Statistics', headings: ['Symbol', 'Average WPM', 'Average Accuracy %', 'Symbol Count'], rows: @rows
+    table = Terminal::Table.new title: 'Your Typing Statistics', headings: ['Symbol', 'Average WPM', 'Average Accuracy %', 'Symbol Count', 'Hit/Miss'], rows: @rows
+    table.align_column(0, :center)
+    table.align_column(1, :center)
+    table.align_column(2, :center)
+    table.align_column(3, :center)
     puts table
   end
 
